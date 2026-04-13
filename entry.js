e14835c1f8c2fdbd002e483a18faf29020994b5b
shc0743 <shc0743@outlook.com>
2026-04-14 03:18:14 +0800

    refactor(app): refactor the project structure

--- entry.js
+++ entry.js
@@ -1,22 +1,52 @@
 #!/usr/bin/env node
 import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-const action = process.argv[2]
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const CLIENT_PY = join(__dirname, 'client.py');
+
+function runPythonClient(args) {
+    const child = spawn('python', [CLIENT_PY, ...args], { stdio: 'inherit' });
+    child.on('exit', (code) => process.exit(code));
+}
+
+const action = process.argv[2];
+const restArgs = process.argv.slice(3);
 
 switch (action) {
-    case "serve":
-        spawn('python', ['client.py', '--type=manager', ...process.argv.slice(3)], {
-            stdio: 'inherit'
-        }).on('exit', (code) => {
-            process.exit(code)
-        });
+    case '--':
+        runPythonClient(restArgs);
+        break;
+
+    case 'serve':
+        runPythonClient(['--type=manager', ...restArgs]);
+        break;
+
+    case 'run':
+        runPythonClient(restArgs);
+        break;
+
+    case 'kill':
+    case 'stop':
+        runPythonClient(['--kill', ...restArgs]);
+        break;
+
+    case 'helpclient':
+        runPythonClient(['--help', ...restArgs]);
         break;
 
     default:
-        console.error("Usage: npx remote-process-server ACTION\n\n" +
-            "Available actions:\n" +
-            "  serve: Start the server process\n"
-        );
+        console.error(`Usage: npx remote-process-server ACTION
+
+Available actions:
+  --           Forward the remaining arguments to the Python client as is
+  serve        Start the server process
+  run          Run something
+  stop         Stop the server
+  kill         Alias for 'stop'
+  helpclient   Show the help of the Python client
+`);
         process.exit(1);
 }
-
--- sys_name.py
+++ sys_name.py
@@ -1,2 +1,3 @@
+#!/usr/bin/env python3
 import platform
 print(str(platform.system().lower()) + '_' + str(platform.machine().lower()))
