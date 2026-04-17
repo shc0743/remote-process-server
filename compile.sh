9e1c683b3d571df907072910148b2593c6fb2edf
shc0743 <shc0743@outlook.com>
2026-04-18 02:49:07 +0800

    perf(server): add optimize compiler options and optimize compile steps

--- compile.sh
+++ compile.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
-SYS_NAME=$(python3 ./sys_name.py)
-if [[ "$SYS_NAME" == android* ]]; then
-    clang++ -std=c++20 server.cpp -I. -o "rmpsm_server.$SYS_NAME"
+if [[ -n "$1" ]]; then
+    SYS_NAME="$1"
 else
-    clang++ -std=c++20 -static -static-libstdc++ -static-libgcc server.cpp -I. -o "rmpsm_server.$SYS_NAME"
+    SYS_NAME=$(python3 ./sys_name.py)
 fi
+if [[ -n "$2" ]]; then
+    COMPILER="$2"
+else
+    COMPILER=$(which clang++)
+fi
+if [[ -n "$3" ]]; then
+    STRIPPER="$3"
+else
+    STRIPPER=$(which strip)
+fi
+if [[ "$SYS_NAME" == android* ]]; then
+    $COMPILER -std=c++20 -O3 -flto server.cpp -I. -o "rmpsm_server.$SYS_NAME" || exit 1
+else
+    $COMPILER -std=c++20 -O3 -flto -static -static-libstdc++ -static-libgcc server.cpp -I. -o "rmpsm_server.$SYS_NAME" || exit 1
+fi
+$STRIPPER "rmpsm_server.$SYS_NAME"
